84c9dd4cfc1dfd4f4c3c727257734b0e8032a782
cc-didic <cc.cedric.caron@gmail.com>
2025-12-29 17:41:30 +0100

    ajout de la section Projets connecté en BDD

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once 'db/db_connect.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -17,11 +18,43 @@
                 <div>Envoyez-moi votre idée de projet, et je vous la réalise en deux semaines.</div>
             </div>
             
-            <img src="img/tom-cruise.jpeg" alt="Photo de Tom Cruise">
+            <img src="img/tom-cruise.jpeg" alt="Tom Cruise">
         </section>
 
         <section class="projects">
+            <h2>Mes projets</h2>
+            <div class="list-projects">
+                <?php
+                    $pdo = getDBConnection();
 
+                    $sql = "SELECT * FROM my_portfolio_php.projects;";
+
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute();
+
+                    $result = $stmt->fetchAll();
+                    foreach($result as $row) :?>
+
+                        <article class="project">
+                            <!-- Images -->
+                            <!-- Titre -->
+                            <h3>
+                                <?php echo htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8') . "\t"; ?>
+                            </h3>
+                            <!-- Description -->
+                            <p class="description">
+                                <?php echo htmlspecialchars($row['description'], ENT_QUOTES, 'UTF-8') . "\t"; ?>
+                            </p>
+                            <!-- Lien Github -->
+                            <a href="<?php echo htmlspecialchars($row['github_link'], ENT_QUOTES, 'UTF-8') . "\n"; ?>">Github</a>
+                            <!-- Lien Projet -->
+                            <a href="<?php echo htmlspecialchars($row['project_link'], ENT_QUOTES, 'UTF-8') . "\n"; ?>">Voir</a>
+                            <!-- Technos -->
+                        </article>
+                        
+                    <?php endforeach; ?>
+                
+            </div>
         </section>
 
         <section class="skills">
