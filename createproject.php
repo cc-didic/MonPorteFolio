f56b04681ce6fa0d5810990d13f012768e14f754
cc-didic <cc.cedric.caron@gmail.com>
2026-01-01 10:10:27 +0100

    CreateProject in BDD

--- createproject.php
+++ createproject.php
@@ -1,3 +1,22 @@
+<?php
+    require_once 'db/function.php';
+
+    $message = null;
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        // Cas où le formulaire a été rempli et soumis
+        $titre = $_POST["title"];
+        $description = $_POST["description"];
+        $github_link = $_POST["github_link"];
+        $project_link = $_POST["project_link"];
+
+        $success = insertProject($titre, $description, $github_link, $project_link);
+    }
+    else{
+        // Cas où le formulaire est vide
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,11 +25,30 @@
     <title>Créer un projet</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
+    <?php require_once "includes/header.php"; ?>
+
     <div class="container">
         <h1 class="section-title">Créer un projet</h1>
 
         <form class="form-card" action="" method="post" enctype="multipart/form-data">
+            
+            <?php
+                if(isset($success)):
+                    if($success):?>
+                        <div class="alert success">
+                            Le projet a bien été créé
+                        </div>
+                    <?php else: ?>
+                        <div class="alert error">
+                            Une erreur est survenue
+                        </div>
+                    <?php endif;
+                endif;
+            ?>
+            
+
             <div class="form-grid">
                 <div class="form-group full">
                     <label for="title">Titre</label>
@@ -24,12 +62,12 @@
 
                 <div class="form-group full">
                     <label for="gh_link">Lien GitHub</label>
-                    <input id="gh_link" name="gh_link" type="url" placeholder="https://github.com/monrepo">
+                    <input id="github_link" name="github_link" type="url" placeholder="https://github.com/monrepo">
                 </div>
 
                 <div class="form-group full">
                     <label for="pr_link">Lien démo / Page</label>
-                    <input id="pr_link" name="pr_link" type="url" placeholder="https://monprojet.example">
+                    <input id="project_link" name="project_link" type="url" placeholder="https://monprojet.example">
                 </div>
             </div>
 
@@ -39,5 +77,6 @@
         </form>
     </div>
     
+    <?php include_once "includes/footer.php"; ?>
 </body>
 </html>
